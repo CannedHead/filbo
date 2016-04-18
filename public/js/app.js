1f3fad850abd4c25985405360031666c1be836f4
folleselavida <julian@cannedhead.com>
2016-04-18 07:00:28 -0500

    safari color

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,12 @@
+var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+   var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+   var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+   var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+   var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+   if ((is_chrome)&&(is_safari)) {is_safari=false;}
+   if ((is_chrome)&&(is_opera)) {is_chrome=false;}
 
+if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {alert('Its Safari');}
 var content = [
 	{
 		"title":"Vuelve la vivienda a Bogotá",
