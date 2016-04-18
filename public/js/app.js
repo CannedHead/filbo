e17ded7a841504ac6c14ef04e1156a014c4b4c80
jdflorez39 <jd.florez39@gmail.com>
2016-04-18 01:49:18 -0500

    video

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,8 @@ var content = [
 		"text":"187 mil nuevas viviendas + Ciudad Norte, una ciudad única en el mundo",
 		"icon":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/1.svg",
 		"count":"12345",
+		"animation":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/video1",
+		"poster":"https://s3-sa-east-1.amazonaws.com/cannedhead.canned/filbo/poster.png",
 		"videourl":'<iframe src="https://player.vimeo.com/video/163168624?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
 		"facebookurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
 		"twitterurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
@@ -14,6 +16,8 @@ var content = [
 		"text":"Un circuito que le dará un carácter sin igual a la ciudad: conformado por 1) El parque de San Rafael – 2) El sendero ecológico y cortafuegos de los Cerros Orientales – 3) El parque lineal Tunjuelo – 4) El gran malecón del Río Bogotá.",
 		"icon":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/2.svg",
 		"count":"12345",
+		"animation":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/video1",
+		"poster":"https://s3-sa-east-1.amazonaws.com/cannedhead.canned/filbo/poster.png",
 		"video-url":'<iframe src="https://player.vimeo.com/video/163180113?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
 		"facebookurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
 		"twitterurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
@@ -24,6 +28,8 @@ var content = [
 		"text":"6 nuevos hospitales + 40 nuevos Centros de Atención Prioritaria en Salud",
 		"icon":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/3.svg",
 		"count":"12345",
+		"animation":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/video1",
+		"poster":"https://s3-sa-east-1.amazonaws.com/cannedhead.canned/filbo/poster.png",
 		"video-url":'<iframe src="https://player.vimeo.com/video/163168624?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
 		"facebookurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
 		"twitterurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
@@ -34,6 +40,8 @@ var content = [
 		"text":"207 kilómetros de nuevas troncales de Transmilenio (hoy hay 120 kilómetros) + ampliación de la Autopista Norte + ampliación de la 7ª",
 		"icon":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/4.svg",
 		"count":"12345",
+		"animation":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/video1",
+		"poster":"https://s3-sa-east-1.amazonaws.com/cannedhead.canned/filbo/poster.png",
 		"video-url":'<iframe src="https://player.vimeo.com/video/163168624?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
 		"facebookurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
 		"twitterurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
@@ -44,6 +52,8 @@ var content = [
 		"text":"45 parques de barrio + 5 parques zonales",
 		"icon":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/5.svg",
 		"count":"12345",
+		"animation":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/video1",
+		"poster":"https://s3-sa-east-1.amazonaws.com/cannedhead.canned/filbo/poster.png",
 		"videourl":'<iframe src="https://player.vimeo.com/video/163187653?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
 		"facebookurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
 		"twitterurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
@@ -54,6 +64,8 @@ var content = [
 		"text":"Más allá de una solución a la movilidad, el metro es un motor de renovación urbanística.",
 		"icon":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/6.svg",
 		"count":"12345",
+		"animation":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/video1",
+		"poster":"https://s3-sa-east-1.amazonaws.com/cannedhead.canned/filbo/poster.png",
 		"videourl":'<iframe src="https://player.vimeo.com/video/163168624?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
 		"facebookurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
 		"twitterurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
@@ -64,6 +76,8 @@ var content = [
 		"text":"30 nuevos colegios + 4 nuevas sedes del Sena + nuevas bibliotecas",
 		"icon":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/7.svg",
 		"count":"12345",
+		"animation":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/video1",
+		"poster":"https://s3-sa-east-1.amazonaws.com/cannedhead.canned/filbo/poster.png",
 		"videourl":'<iframe src="https://player.vimeo.com/video/163176061?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
 		"facebookurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
 		"twitterurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
@@ -74,6 +88,8 @@ var content = [
 		"text":"Nuevo Centro de Comando y Control + cámaras en toda la ciudad.",
 		"icon":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/8.svg",
 		"count":"12345",
+		"animation":"http://d1q7w1k8zlumh6.cloudfront.net/filbo/video1",
+		"poster":"https://s3-sa-east-1.amazonaws.com/cannedhead.canned/filbo/poster.png",
 		"videourl":'<iframe src="https://player.vimeo.com/video/163176953?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
 		"facebookurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
 		"twitterurl":"http://www.facebook.com/sharer.php?s=100&p[title]=Aca titulo&p[summary]=aca descripcion&p[url]=http://filbo.herokuapp.com&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT",
@@ -89,6 +105,7 @@ $("#icons-wrapper button").click(function(){
 	$(this).data('id');
     $("#plan-info").toggleClass('hidden');
     $(this).addClass("active");
+    fadeVideos(0, 1);
 });
 
 $('#myModal').on('show.bs.modal', function (event) {
@@ -108,4 +125,38 @@ $('#myModal').on('show.bs.modal', function (event) {
     };
 
     var counter2 = new flipCounter('counter2', defaults);
-})
+});
+
+
+/**
+  * Add all videos to given.
+  */
+function loadVideos(videos){
+	for(var i=0;i<videos.length;i++){
+		loadVideoHTML('video-container',videos[i].animation,i,videos[i].poster);
+	}
+	$("#video0").css("display","block");
+}
+
+/**
+  * Creates video HTML tag and adds video to the given container.
+  */
+function loadVideoHTML(container, video, videoid, imgposter){
+	console.log(video);
+	$("#"+container).prepend('<video id="video'+videoid+'" class="img-responsive" preload="metadata" poster="'+imgposter+'" style="display:none;"></video>');
+	$("#video"+videoid).append('<source src="'+video+'.mp4">');
+	$("#video"+videoid).append('<source src="'+video+'.ogg">');
+	$("#video"+videoid).append('<source src="'+video+'.webm">');
+}
+
+/*
+ * Fade between two videos
+ */
+function fadeVideos(from , to){
+	$("#video"+from).css("display","none");
+	$("#video"+to).css("display","block");
+	document.getElementById("video"+to).play();
+}
+
+loadVideos(content);
+
